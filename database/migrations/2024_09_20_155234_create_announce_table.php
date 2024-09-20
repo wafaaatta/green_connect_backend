cfaef5475251d48327a12b1d4dbc32ea43da13e8
wafaa <wafaaatta04@gmail.com>
2024-09-20 19:07:52 +0300

    créé le modèle et le tableau pour les annonces

---
+++ database/migrations/2024_09_20_155234_create_announce_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('announces', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('location');
+            $table->enum('status', ['pending', 'accepted', 'rejected']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('announce_requests');
+    }
+};
